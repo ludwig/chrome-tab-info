3ea1e9523267522d996088229ff1f29370841465
Luis Armendariz <luis.armendariz@gmail.com>
2023-05-27 19:08:41 -0700

    Fix scrolling issue

--- context_script.js
+++ context_script.js
@@ -1,11 +1,15 @@
 // context_script.js
 function copyToClipboard(text) {
   var input = document.createElement('textarea');
+  // Save current scroll position
+  var scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
   document.body.appendChild(input);
   input.value = text;
   input.focus();
   input.select();
   document.execCommand('Copy');
+  // Restore scroll position
+  window.scrollTo(0, scrollPosition);
   document.body.removeChild(input);
 }
 
