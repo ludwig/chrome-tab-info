98c576773b610675ef90971168143331949cff9f
Luis Armendariz <luis.armendariz@gmail.com>
2023-05-27 19:31:56 -0700

    Refactor generation of text

--- background.js
+++ background.js
@@ -9,12 +9,11 @@ chrome.runtime.onInstalled.addListener(() => {
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
   if (info.menuItemId === 'copyTabInfo') {
-    const text = `${tab.title}\n${tab.url}`;
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
       files: ['context_script.js']
     }).then(() => {
-      chrome.tabs.sendMessage(tab.id, {action: "copy", text: text});
+      chrome.tabs.sendMessage(tab.id, {action: "copy", title: tab.title, url: tab.url, useHtml: true});
     });
   }
 });
@@ -23,12 +22,11 @@ chrome.commands.onCommand.addListener((command) => {
   if (command === 'copy_tab_info') {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       const tab = tabs[0];
-      const text = `${tab.title}\n${tab.url}`;
       chrome.scripting.executeScript({
         target: { tabId: tab.id },
         files: ['context_script.js']
       }).then(() => {
-        chrome.tabs.sendMessage(tab.id, {action: "copy", text: text});
+        chrome.tabs.sendMessage(tab.id, {action: "copy", title: tab.title, url: tab.url, useHtml: true});
       });
     });
   }
--- context_script.js
+++ context_script.js
@@ -1,22 +1,27 @@
 // context_script.js
-function copyToClipboard(text) {
-  var input = document.createElement('textarea');
-  // Save current scroll position
+function copyToClipboard(title, url, useHtml) {
   var scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-  document.body.appendChild(input);
-  input.value = text;
-  input.focus();
-  input.select();
-  document.execCommand('Copy');
-  // Restore scroll position
+  if (useHtml) {
+    // Copy HTML to clipboard
+    //const html = `<a href="${url}">${title}</a>`;
+    const html2 = `<p>${title}</p>\n<a href="${url}">${url}</a>\n`;
+    navigator.clipboard.write([
+      new ClipboardItem({
+        "text/plain": new Blob([`${title}\n${url}`], { type: "text/plain" }),
+        "text/html": new Blob([html2], { type: "text/html" })
+      })
+    ]);
+  } else {
+    // Copy plain text to clipboard
+    navigator.clipboard.writeText(`${title}\n${url}`);
+  }
   window.scrollTo(0, scrollPosition);
-  document.body.removeChild(input);
 }
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if (request.action == "copy") {
-      copyToClipboard(request.text);
+      copyToClipboard(request.title, request.url, request.useHtml);
     }
   }
 );
